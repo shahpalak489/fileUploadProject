83e436618a330a594a0cb8cf48ad38f87c7806cd
unknown <shah.palak489@gmail.com>
2021-07-25 13:37:36 -0400

    connection var added to the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 import os
 from app import config
 import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 
 engine = sqlalchemy.create_engine('mssql+pyodbc://'+ 
                      os.environ["DB_SERVER"] + '/' + 
                      os.environ["DB_NAME"] + '?driver=' + 
                      os.environ["DB_DRIVER"])
-                     
+connection = engine.connect()
+session = sessionmaker(engine)    
+
 # import pyodbc 
 # conn = pyodbc.connect('Driver={SQL Server};'
 #                      'Server=LAPTOP-QDH3PMIF;'
