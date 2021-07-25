42dad981f40633ccaaa4eb7c931ba45c61ef32e5
unknown <shah.palak489@gmail.com>
2021-07-25 10:34:48 -0400

    sql and static folder added

---
+++ app/misc/sql/get_company_info.sql
@@ -0,0 +1,9 @@
+select 
+    id,
+	cname,
+	convert(datetime, share_price_dt, 23) share_price_dt,
+	convert(numeric(19, 2), share_price) share_price,
+	comments,
+	f_name
+from 
+    master.dbo.company_info_v2
---
+++ app/misc/sql/get_unique_cid_cname.sql
@@ -0,0 +1,5 @@
+select 
+   distinct cid, cname
+from
+   master.dbo.company_info_v2
+   
