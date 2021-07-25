f404abf445a045d7706c0cb5f916a0d36bdd7a29
unknown <shah.palak489@gmail.com>
2021-07-25 10:36:47 -0400

    new blueprint added and static folder path added

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import os
 from flask import Flask
 from app.misc.views import misc_blueprint
+from app.misc.company import com_blueprint
+from app import config
 
-TEMPLATE_DIR = os.path.abspath('app/templates')
-app = Flask(__name__, template_folder=TEMPLATE_DIR)
+TEMPLATE_DIR = os.path.abspath(os.environ["TEMPLATE"])
+STATIC_DIR = os.path.abspath(os.environ["STATIC"])
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.register_blueprint(misc_blueprint)
+app.register_blueprint(com_blueprint)
 
 @app.route("/")
 def welcome():
