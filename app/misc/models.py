7d836c16be09499f6558578331a01df76e3008ba
unknown <shah.palak489@gmail.com>
2021-07-25 14:13:55 -0400

    model created for sqlalchemy

--- app/misc/models.py
+++ app/misc/models.py
@@ -1,4 +1,4 @@
-# from sqlalchemy import Column, Date, Integer, Boolean, String, *
+# from sqlalchemy import Column, Date, Integer, Boolean, String, Numeric, DateTime, Boolean
 # from sqlalchemy import create_engine, MetaData
 # from sqlalchemy.orm import scoped_session, sessionmaker
 # from sqlalchemy.ext.declarative import declarative_base
@@ -6,17 +6,18 @@
 # Base = declarative_base()
 
 # class cCompanyInfo(Base):
-# 	__tablename__   = "company_info"
-# 	__table_args__ = {'schmema' : 'dbo'}
-# 	id = Column(Integer, primery_key = True)
-# 	cname = Column(String(64), primery_key = True)
-# 	share_price_dt = Column(Date)
-# 	share_price = Column(String(64))  # float
-# 	comments = Column(String(64))
-# 	file_name = Column(String(64))
-# 	inserted_by = Column(String(64))
-# 	inserted_time = Column(DateTime)
-#     canceled_by = Column(String(64))
-# 	canceled_time = Column(DateTime)
-# 	canceled = Column(Bit)
-#     runid = Column(Numeric(20,0))
+#     __tablename__   = "company_info_v2"
+#     __table_args__ = {'schmema' : 'dbo'}
+#     id = Column(Integer, primary_key = True)
+#     cid = Column(Integer)
+#     cname = Column(String(64))
+#     # share_price_dt = Column(Date)
+#     # share_price = Column(Numeric(20,2))
+#     # comments = Column(String(64))
+#     # file_name = Column(String(64))
+#     # inserted_by = Column(String(64))
+#     # inserted_time = Column(DateTime)
+#     # canceled_by = Column(String(64))
+#     # canceled_time = Column(DateTime)
+#     # canceled = Column(Boolean)
+#     # runid = Column(Numeric(20,0))
