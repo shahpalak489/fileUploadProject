aa4294803831893f2a3267761e67837485dbcc8e
unknown <shah.palak489@gmail.com>
2021-07-26 09:00:39 -0400

    comments removed

--- app/__init__.py
+++ app/__init__.py
@@ -19,11 +19,4 @@ engine = sqlalchemy.create_engine('mssql+pyodbc://'+
                      os.environ["DB_NAME"] + '?driver=' + 
                      os.environ["DB_DRIVER"])
 connection = engine.connect()
-session = sessionmaker(engine)    
-
-# import pyodbc 
-# conn = pyodbc.connect('Driver={SQL Server};'
-#                      'Server=LAPTOP-QDH3PMIF;'
-#                      'Database=master;'
-#                      'Trusted_Connection=yes;')
-# cursor = conn.cursor()
+session = sessionmaker(engine)
